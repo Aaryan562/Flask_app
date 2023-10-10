7cc7f434a6f0ad89b69eccf9a54eb36ad30e3b52
Aaryan562 <aaryanclever555@gmail.com>
2023-10-10 23:50:06 +0530

    Added templates for webpage

--- app.py
+++ app.py
@@ -1,11 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Engineering Manager (8+ years)',
+    'location': 'Bengaluru, India',
+    'type': 'Full-Time'
+  },
+  {
+    'id': 2,
+    'title': 'Senior Python Engineer (4+ years)',
+    'location': 'Bengaluru, India',
+    'type': 'Full-Time'
+  },
+  {
+    'id': 3,
+    'title': 'Python Interns (Mumbai)',
+    'location': 'Mumbai, India',
+    'type': 'Hybrid'
+  }
+]
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def hello_docsumo():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Docsumo')
 
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
-if __name__=="__main__":
-    app.run(host="0.0.0.0",debug=True)
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
